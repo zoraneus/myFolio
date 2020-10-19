76170522e10698ea46c8aecdce303957faac4f40
Guilian Ganster <guilian.ganster@gmail.com>
2020-10-19 23:37:37 +0200

    baneer to children

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -4,8 +4,10 @@ import {Baneer} from '../Components/BaneerSection';
 const HomePage = () => (
   <div>
     <Baneer>
-      
+      <h1>Guilian Ganster</h1>
+      <p>Etudiant Epitech 3e année (Master MSC 5 ans) et développeur freelance fullstack depuis 1 ans. Je cherche un contrat professionnel 3j/semaine de janvier jusqu’à août 2021 à Rennes,  (renouvelable sur 2 ans).</p>
     </Baneer>
+    
   </div>
 )
 
