04a059af578760e68f11de2e389a15e198fe261d
Guilian Ganster <guilian.ganster@gmail.com>
2020-10-20 00:38:59 +0200

    progressbar

--- src/Layouts/main.js
+++ src/Layouts/main.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import {Navbar} from '../Components/Navbar';
+import ProgressBar from 'react-scroll-progress-bar';
 import {Sidebar, SidebarToggle} from '../Components/Sidebar'
 import SidebarContext from '../Contexts/sidebar.context';
 
@@ -18,6 +19,7 @@ const MainLayout = ({render}) => {
       <div className={styles.MainLayoutContent}>
         {render}
       </div>
+      <ProgressBar />
       <Navbar />
       <Sidebar />
     </SidebarContext.Provider>
